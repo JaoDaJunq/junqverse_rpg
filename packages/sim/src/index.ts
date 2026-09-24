9b7e30696ba612d91abc7f5c04eb08d3a0c1cd39
JaoDaJunq <jpachecohoppe@gmail.com>
2026-09-24 10:42:19 -0300

    T018: implementar núcleo de objetivos básicos

--- packages/sim/src/index.ts
+++ packages/sim/src/index.ts
@@ -18,3 +18,4 @@ export * from './heroes/jao-advanced.js';
 export * from './ai/pathfinding.js';
 export * from './ai/fsm.js';
 export * from './encounter.js';
+export * from './quests/objectives.js';
